894f7779926dc9e0433a315d6adc850ac3b31b35
Andrew <AND2181591@maricopa.edu>
2022-03-18 21:14:21 -0700

    Completed section 4 Creating the List Page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
